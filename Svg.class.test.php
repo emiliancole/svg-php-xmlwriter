b4795c1b613770ae97e66f37a02abbafed9dcec1
Emilian Cole <emiliancole@gmail.com>
2019-07-09 12:49:01 +0200

    Add files via upload

---
+++ Svg.class.test.php
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<svg viewBox="0 0 500 500">
+ <symbol id="sym01">
+  <circle id="" cx="20" cy="20" r="5" style="STYLE100"/>
+ </symbol>
+ <use x="100" y="100" width="100" height="50" href="#sym01" style="fill:red;stroke:black;stroke-width:1;opacity:1.0"/>
+ <use x="120" y="100" width="100" height="50" href="#sym01" style="fill:red;stroke:black;stroke-width:1;opacity:0.8"/>
+ <use x="140" y="100" width="100" height="50" href="#sym01" style="fill:red;stroke:black;stroke-width:1;opacity:0.6"/>
+ <use x="160" y="100" width="100" height="50" href="#sym01" style="fill:red;stroke:black;stroke-width:1;opacity:0.4"/>
+ <g id="rect01" style="">
+  <rect x="50" y="50" width="30" height="30" rx="5" ry="5" style="fill:red;stroke:black;stroke-width:1;"/>
+ </g>
+ <path id="path01" d="M10,90 Q90,90 90,45 Q90,10 50,10 Q10,10 10,40 Q10,70 45,70 Q70,70 75,50" style="fill:none;stroke:black;stroke-width:1;"/>
+ <path id="" d="M100 100 A30 30 0 0 0 120 150" style="fill:none;stroke:black;stroke-width:1;"/>
+ <text>
+  <textPath href="#path01" dx="" dy="" style="fill:none;stroke:black;stroke-width:1;">text on path</textPath>
+ </text>
+</svg>
+<hr>Array
+(
+    [0] => 
+    [1] => M100 100 A30 30 0 0 0 120 150
+)
+
